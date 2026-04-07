9e3465f3fc340087b49b2df79fd7fbbeb4678338
Aryan2002-max <bhadoriyaaryansingh35@gmail.com>
2026-04-07 13:15:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,8 @@ def get_bot_response():
     response = chatbot("user1", user_msg)
     return jsonify({"response": response})
 
+import os
+
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0" , port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
